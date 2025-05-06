f151f7bd4d1dcb1728fdb8e6c78b89dd07a8907c
Chinaskidev <chinaskicharles0@gmail.com>
2025-05-06 16:05:09 -0600

    enpoint de perfiles y tablas de candidatos y niveles de profesion

--- app/database.py
+++ app/database.py
@@ -82,16 +82,27 @@ class Analize(Base):
     
     
 
-# # tabla del  candidato
-# class Candidate(Base):
-#     __tablename__ = "candidatos"
-#     id = Column(Integer, primary_key=True, index=True)
-#     name = Column(String, nullable=False)
-#     job_id = Column(Integer, ForeignKey("tipos_de_trabajo.id", ondelete="CASCADE"), nullable=False)
+# tabla del  candidato
+class Candidate(Base):
+     __tablename__ = "candidatos"
+     id = Column(Integer, primary_key=True, index=True)
+     name = Column(String, nullable=False)
+     birthday = Column(String, nullable=False)
+     country = Column(String, nullable=False)
+     bio = Column(String, nullable=False)
+     nivel_id = Column(Integer, ForeignKey("tipos_de_profesiones.id", ondelete="CASCADE"), nullable=False)
     
-#     # Se relaciona con el trabajo.
-#     job = relationship("Job", back_populates="candidates")
+#  Se relaciona con el trabajo.
+     nivel = relationship("Nivel", back_populates="candidatos")
     
+    
+# tabla del  nivel de profesion/educacion
+class Nivel(Base):
+    __tablename__ = "tipos_de_profesiones"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    # Relación inversa con la tabla Candidate
+    candidates = relationship("Candidate", back_populates="Nivel")
     
 # contactos
 class Contact(Base):
