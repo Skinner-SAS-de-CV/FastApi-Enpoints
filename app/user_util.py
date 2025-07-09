a74d13eeadc6b435c1bd0ff0b481d95405b602cd
William Lemus <william.lemus@gmail.com>
2025-07-09 11:35:05 -0600

    issue-62: remover endpoints GET perfiles, clientes y trabajos

--- app/user_util.py
+++
@@ -1,9 +0,0 @@
-import os
-from clerk_backend_api import Clerk
-
-def onboard_user(clerk_user_id=str, user_id=str):
-    sdk =Clerk(
-        bearer_auth=os.getenv('CLERK_SECRET_KEY'),
-    )
-    res = sdk.users.update(user_id=clerk_user_id, external_id=user_id, public_metadata={ "onboardingComplete": True})
-    return res
